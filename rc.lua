38cc42ddf2212980f9cdab3a394f19665ab8bd6a
Tanguy Gérôme <tanguy.gerome@gmail.com>
2020-12-01 13:42:08 +0100

    altkey fix

--- rc.lua
+++ rc.lua
@@ -76,6 +76,7 @@ editor_cmd = terminal .. " -e " .. editor
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
+altkey = "Mod1"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
